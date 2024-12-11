4d3b2e879974b14f95de2138c0fd068c78cf6625
thiramet27 <thirametphuthong@gmail.com>
2024-12-12 04:02:27 +0700

    Update config

--- server.js
+++ server.js
@@ -3,28 +3,25 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
-import connectCloudinary from "./config/cloudinary.js";
 import productRoutes from "./routes/productRoutes.js";
-import adminRoutes from './routes/adminRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import cartRoutes from './routes/cartRoutes.js';
+import adminRoutes from "./routes/adminRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
 
-const allowedOrigins = [
-    "http://localhost:5174",
-    "http://localhost:5173",
-    "https://orbit-rex-living.vercel.app/",
-];
+// Get allowedOrigins from .env and split into an array
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+    ? process.env.ALLOWED_ORIGINS.split(",")
+    : []; // Default to an empty array if not set
 
 // Configure CORS
 app.use(
     cors({
         origin: (origin, callback) => {
-            if (!origin) return callback(null, true);
+            if (!origin) return callback(null, true); // Allow requests without origin (e.g., mobile apps)
             if (allowedOrigins.includes(origin)) {
                 callback(null, true);
             } else {
@@ -37,7 +34,6 @@ app.use(
 );
 
 connectDB();
-connectCloudinary()
 
 app.use(express.json());
 
@@ -45,11 +41,11 @@ app.get("/", (req, res) => {
     res.send("API Working");
 });
 
-app.use("/api",productRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/order', orderRoutes);
-app.use('/api/cart', cartRoutes);
+app.use("/api", productRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
